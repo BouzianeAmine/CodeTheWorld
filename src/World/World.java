d88948782d239673c8ae1e211754bf50940f3792
amine <aminebouz84@gmail.com>
2019-05-19 10:04:59 +0200

    Still on dummy coding

--- src/World/World.java
+++ src/World/World.java
@@ -1,5 +1,10 @@
 package World;
 
 public class World {
-
+private God god;
+private Universe universe;
+public Planete planete;
+public Humans humans;
+public Animals animals;
+public Nature nature;
 }
